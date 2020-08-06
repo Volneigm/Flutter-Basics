55f1caa3b13e5e92c5ea35468a2e551e889b6530
Volneigm <33768750+Volneigm@users.noreply.github.com>
2020-08-06 20:15:36 -0300

    I Am Poor

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image(image: AssetImage('images/i_am_poor.png')),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[500],
+          title: Text('I Am Poor'),
+        ),
+      ),
+    );
+  }
+}
