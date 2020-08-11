b11e4c3ccf7899436bf8682bc65d3c70cf6759c6
Volneigm <33768750+Volneigm@users.noreply.github.com>
2020-08-10 23:09:04 -0300

    Mi Card Flutter

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,10 +13,79 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundImage: AssetImage('images/flutter_icon.png'),
+                backgroundImage: AssetImage('images/flutter_icon.jpg'),
+              ),
+              Text(
+                'Volnei M',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.grey[300],
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 0.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 25.0,
+                    ),
+                    SizedBox(
+                      width: 30.0,
+                    ),
+                    Text(
+                        '+55 99887766',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 30.0,
+                    ),
+                    Text(
+                      'volnei@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
