44e2f8fbdf461f5f48fdb7927517768f18a0b476
Volneigm <33768750+Volneigm@users.noreply.github.com>
2020-09-06 00:16:09 -0300

    Campo Minado App

--- flutter_campo_minado/lib/sceens/campo_minado_app.dart
+++ flutter_campo_minado/lib/sceens/campo_minado_app.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Campo Minado'),
-        ),
+        appBar: ResultadoWidget(),
         body: Container(
           child: Text('Tabuleiro'),
         ),
